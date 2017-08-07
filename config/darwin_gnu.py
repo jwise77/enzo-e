1edfa2a60eba1f454b098275512b9abeac5b977d
John Wise <jwise@physics.gatech.edu>
2017-08-07 10:43:25 -0700

    Changes to darwin config file

--- config/darwin_gnu.py
+++ config/darwin_gnu.py
@@ -2,14 +2,14 @@ import os
 
 is_arch_valid = 1
 
-flags_arch = '-O3 -g'
+flags_arch = '-g'
 #flags_arch = '-Wall -pg -fprofile-arcs -ftest-coverage'
 #flags_arch = '-Wall -pg'
 #flags_arch = '-Wall -g'
-flags_link_charm = ' -rdynamic' # required for backtraces
+flags_link_charm = '' # required for backtraces
 
-cc  = 'gcc-mp-4.9'
-f90 = 'gfortran-mp-4.9'
+cc  = 'gcc'
+f90 = 'gfortran'
 
 flags_prec_single = '-fdefault-real-4 -fdefault-double-8'
 flags_prec_double = '-fdefault-real-8 -fdefault-double-8'
@@ -19,7 +19,7 @@ libs_fortran    = ['gfortran']
 
 home = os.environ['HOME']
 
-charm_path = home + '/codes/charm-6.5.1'
+charm_path = home + '/codes/charm'
 papi_inc   = '/usr/local/include'
 papi_lib   = '/usr/local/lib'
 hdf5_inc   = '/opt/local/include'
